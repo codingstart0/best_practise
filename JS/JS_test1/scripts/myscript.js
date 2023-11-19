827221f21e36debe7a83ccd1420d0547d967d7f5
codingstart0 <codingstart0@gmail.com>
2023-11-19 09:42:13 +0200

    Remove commented code

--- JS/JS_test1/scripts/myscript.js
+++ JS/JS_test1/scripts/myscript.js
@@ -1,21 +1,6 @@
 var button = document.querySelector("button");
 var box = document.getElementById("changeMe");
 
-// function changeColor() {
-//   // box.style.background = "blue";
-
-//   if (box.style.background == "red") {
-//      box.style.background = "green";
-//   } else {
-//     box.style.background = "hotpink";
-//   }
-// }
-
-
-// function changeColor(){
-//   box.style.background = "blue";
-// }
-
 function changeColor(){
   if(box.style.background == "red"){
     box.style.background = "green";
