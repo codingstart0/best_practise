ca1accfe953b4579074fd430c0ebb747c1dce3d1
codingstart0 <codingstart0@gmail.com>
2023-11-20 14:37:12 +0200

    SHORT-TEST# Features_ Add asigned declaration

--- JS/short_test/task-variables.js
+++ JS/short_test/task-variables.js
@@ -9,3 +9,15 @@ admin = name;
 alert(admin);
 
 
+const planet = "Earth";
+// my soliution
+
+let ourPlanetName = "Earth";
+// curiculum solution
+
+
+let user = "User_name";
+// my soliution
+
+let curentUserName = "Jim"
+// curiculum solution
