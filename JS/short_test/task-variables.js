bb7729733d65fbf1b980692ff1d054ec8a34d358
codingstart0 <codingstart0@gmail.com>
2023-11-22 06:18:56 +0200

    SHORT-TEST# Features_ Add style to button

--- JS/short_test/task-variables.js
+++ JS/short_test/task-variables.js
@@ -1,23 +1,14 @@
-var button = document.querySelector("button");
-var box = document.getElementById("changeMe");
+const btn = document.querySelector("button");
+const txt = document.querySelector("p");
 
+btn.addEventListener("click", updateBtn);
 
-let admin;
-let name = "Rohn";
-admin = name;
-
-alert(admin);
-
-
-const planet = "Earth";
-// my soliution
-
-let ourPlanetName = "Earth";
-// curiculum solution
-
-
-let user = "User_name";
-// my soliution
-
-let curentUserName = "Jim"
-// curiculum solution
+function updateBtn() {
+  if (btn.textContent === "Start machine") {
+    btn.textContent = "Stop machine";
+    txt.textContent = "The machine has started!";
+  } else {
+    btn.textContent = "Start machine";
+    txt.textContent = "The machine is stopped.";
+  }
+}
