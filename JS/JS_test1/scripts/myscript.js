bc2ca33509ebbd10ec7aaf371d70abb7ca84ce5d
codingstart0 <129733643+codingstart0@users.noreply.github.com>
2023-11-22 09:21:04 +0200

    Update myscript.js
    Delete new changes and leave the old version of myscript.js file.

--- JS/JS_test1/scripts/myscript.js
+++ JS/JS_test1/scripts/myscript.js
@@ -1,30 +1,10 @@
 var button = document.querySelector("button");
 var box = document.getElementById("changeMe");
 
-function changeColor() {
-  if (box.style.background == "red") {
+function changeColor(){
+  if(box.style.background == "red"){
     box.style.background = "green";
-  } else {
+  }else{
     box.style.background = "red";
   }
 }
-
-// let message;
-// message = "Hello!";
-// message = "We will use cooke's" , "Are you ok with this";
-// alert(message);
-
-let message;
-message = "Hello!";
-
-let message2;
-message2 = "Are you ok with this?";
-
-message = "We will use cooke's. ";
-alert(message+message2);
-
-// let hello = "Hello world";
-// let message;
-// message = hello;
-// alert(message);
-// alert(hello);
