1a326f3af4b939ace70715a80af800adaebaed41
codingstart0 <codingstart0@gmail.com>
2023-11-24 04:38:28 +0200

    JS_TEST2#_Add more button

--- JS/JS_test2/task-variables.js
+++ JS/JS_test2/task-variables.js
@@ -1,18 +1,42 @@
-const btn = document.querySelector("button");
-const txt = document.querySelector("h1");
+const btns = document.querySelectorAll(".start-button");
+// const txt = document.querySelector("h1");
 // const bckg = document.querySelector("body");
 
-btn.addEventListener("click", updateBtn);
+const startedButtonClass = "started"
 
-function updateBtn() {
-  if (btn.textContent === "Start machine") {
-    btn.textContent = "Stop machine";
-    txt.textContent = "The machine has started!";
+// btn.addEventListener("click", updateBtn);
+// btns.forEach((btn)=> {
+//   btn.addEventListener("click", updateBtn);
+// })
+
+function startMachine(btn,txt){
+  btn.textContent = "Stop machine"; 
+  txt.textContent = "The machine has started!";
+  btn.classList.add(startedButtonClass);
+  txt.classList.add(startedButtonClass);
+}
+
+function stopMachine (btn,txt){
+  btn.textContent = "Start machine"; 
+  txt.textContent = "The machine is stopped.";
+  // btn.className = "start-button"
+  btn.classList.remove(startedButtonClass);
+  txt.classList.remove(startedButtonClass);
+}
+
+function updateBtn(event) {
+  console.log(event.target);
+  const clickedBtn = event.target;
+  const txt = clickedBtn.nextElementSibling;
+  console.log (clickedBtn.nextElementSibling)
+  if (clickedBtn.textContent === "Start machine") {
+   startMachine(clickedBtn, txt);
+
+    // btn.className = "start-button started"
   } else {
-    btn.textContent = "Start machine";
-    txt.textContent = "The machine is stopped.";
+    stopMachine(clickedBtn,txt);
     // bckg mintis kad cia reikia irasyti komanda
   }
 }
 
-console.log(btn);
+// console.log(btn);
