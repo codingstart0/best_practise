f00c39cb76eee916050f2f037824e82f09e4d4dc
codingstart0 <codingstart0@gmail.com>
2023-12-02 09:52:38 +0200

    short_test#Feature_Add .storyPush

--- JS/short_test/shTest4/script.js
+++ JS/short_test/shTest4/script.js
@@ -1,9 +1,15 @@
 const story = document.body.querySelector(".story");
 
+const storyPush = document.getElementsByClassName(".storyPush");
+
 const setText = document.body.querySelector("#set-text");
-setText.addEventListener("click", () => {
-  story.textContent = "It was a dark and stormy night...";
-});
+setText.addEventListener("click",
+
+() => {
+  story.textContent = "It was a dark and stormy night...", "storyPush";
+}
+
+);
 
 const clearText = document.body.querySelector("#clear-text");
 clearText.addEventListener("click", () => {
