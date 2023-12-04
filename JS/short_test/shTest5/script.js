8354ff353b8dc885cba452b26134682ffeed66db
codingstart0 <codingstart0@gmail.com>
2023-12-04 13:37:35 +0200

    shtest5#Feature_Update "remove-child" button, now on click removes all child from both class ".dad" and ".mom"

--- JS/short_test/shTest5/script.js
+++ JS/short_test/shTest5/script.js
@@ -1,36 +1,37 @@
-const dadBox = document.body.querySelector(".dad");
-const dadButton = document.querySelector("#add-child-dad");
+const dadBox = document.body.querySelector('.dad');
+const dadButton = document.querySelector('#add-child-dad');
 
-dadButton.addEventListener("click", () => {
+dadButton.addEventListener('click', () => {
   // Only add a child if we don't already have one
   // in addition to the text node "parent"
   if (dadBox.childNodes.length > 3) {
     return;
   }
-  const child = document.createElement("div");
-  child.classList.add("child");
-  child.textContent = "child";
+  const child = document.createElement('div');
+  child.classList.add('child');
+  child.textContent = 'child';
   dadBox.appendChild(child);
 });
 
+const momBox = document.body.querySelector('.mom');
+const momButton = document.querySelector('#add-child-mom');
 
-const momBox = document.body.querySelector(".mom");
-const momButton = document.querySelector("#add-child-mom");
-
-momButton.addEventListener("click", () => {
+momButton.addEventListener('click', () => {
   // Only add a child if we don't already have one
   // in addition to the text node "parent"
   if (momBox.childNodes.length > 4) {
     return;
   }
-  const child = document.createElement("div");
-  child.classList.add("child");
-  child.textContent = "child";
+  const child = document.createElement('div');
+  child.classList.add('child');
+  child.textContent = 'child';
   momBox.appendChild(child);
 });
 
-const removeChild = document.body.querySelector("#remove-child");
-removeChild.addEventListener("click", () => {
-  const child = document.body.querySelector(".child");
-  dadBox.removeChild(child);
+const removeChild = document.body.querySelector('#remove-child');
+removeChild.addEventListener('click', () => {
+  const children = document.body.querySelectorAll('.child');
+  children.forEach((child) => {
+    child.parentNode.removeChild(child);
+  });
 });
