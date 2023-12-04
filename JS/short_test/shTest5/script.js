497e10ad1eccdda0a9d987dc6496fc9a2ca98ec4
codingstart0 <codingstart0@gmail.com>
2023-12-04 12:14:43 +0200

    shtest#Feature_Add max lenght of 4 for "dad" class 	Add style for "parents" class

--- JS/short_test/shTest5/script.js
+++ JS/short_test/shTest5/script.js
@@ -1,20 +1,72 @@
-const parent = document.body.querySelector(".parent");
+const dadBox = document.body.querySelector(".dad");
+const dadButton = document.querySelector("#add-child-dad");
 
-const addChild = document.body.querySelector("#add-child");
-addChild.addEventListener("click", () => {
+dadButton.addEventListener("click", () => {
   // Only add a child if we don't already have one
   // in addition to the text node "parent"
-  if (parent.childNodes.length > 1) {
+  if (dadBox.childNodes.length > 4) {
     return;
   }
   const child = document.createElement("div");
   child.classList.add("child");
   child.textContent = "child";
-  parent.appendChild(child);
+  dadBox.appendChild(child);
 });
 
 const removeChild = document.body.querySelector("#remove-child");
 removeChild.addEventListener("click", () => {
   const child = document.body.querySelector(".child");
-  parent.removeChild(child);
+  dadBox.removeChild(child);
 });
+
+
+
+const momBox = document.body.querySelector(".mom");
+const momButton = document.querySelector("#add-child-mom");
+
+
+
+
+
+
+
+////////////////////////////////////////
+
+// const addChild = document.body.querySelector("#add-child-dad");
+// addChild.addEventListener("click", () => {
+//   // Only add a child if we don't already have one
+//   // in addition to the text node "parent"
+// //   if (dad.childNodes.length > 1) {
+// //     return;
+// //   }
+// // if (Event.click) = dad {
+//   const child = document.createElement("div");
+//   child.classList.add("child");
+//   child.textContent = "child for dad";
+//   dadBox.appendChild(child);
+
+// //   const child = document.createElement("div");
+// //   child.classList.add("child");
+// //   child.textContent = "child for mom";
+// //   mom.appendChild(child);
+// });
+
+
+// Child = document.body.querySelector("#add-child-mom");
+// addChild.addEventListener("click", () => {
+//   // Only add a child if we don't already have one
+//   // in addition to the text node "parent"
+//   if (mom.childNodes.length > 1) {
+//     return;
+//   }
+
+// });
+
+
+
+
+
+
+
+
+
