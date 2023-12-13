a7f1dc3daf7813b2a27aff2f262bed0377af117b
codingstart0 <codingstart0@gmail.com>
2023-12-13 15:06:02 +0200

    shtest7#Features_Add basic code stucture

--- JS/short_test/shTest7/script.js
+++ JS/short_test/shTest7/script.js
@@ -1,1 +1,23 @@
-console.log();
+const select = document.querySelector('select');
+const list = document.querySelector('ul');
+const h1 = document.querySelector('h1');
+
+select.addEventListener('change', () => {
+  const choice = select.value;
+
+  // ADD CONDITIONAL HERE
+
+  createCalendar(days, choice);
+});
+
+function createCalendar(days, choice) {
+  list.innerHTML = '';
+  h1.textContent = choice;
+  for (let i = 1; i <= days; i++) {
+    const listItem = document.createElement('li');
+    listItem.textContent = i;
+    list.appendChild(listItem);
+  }
+}
+
+createCalendar(31, 'January');
