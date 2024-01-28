67ec59dd7fc4229541c0636a79d7d48b55205387
codingstart0 <codingstart0@gmail.com>
2024-01-28 08:09:15 +0200

    shTest12#_Bug#_sayHi function doesn't saved

--- JS/short_test/script.js
+++ JS/short_test/script.js
@@ -1,1 +1,35 @@
-console.log();
+// let age = 20;
+// function checkAge(age) {
+//     if (age > 18) {
+//       return true;
+//     } else {
+//       return confirm('Did parents allow you?');
+//     }
+//   }
+// alert(checkAge(age));
+
+//Using a question mark operator ?
+// let age = 10;
+// function checkAge(age) {
+// return (age > 18) ? true : confirm('Did parents allow you?');
+// }
+// alert(checkAge(age));
+
+// Using OR||
+// let age = 8;
+// function checkAge(age) {
+//     return (age > 18) || confirm('Did parents allow you?');}
+// alert(checkAge(age));
+
+// function min(a, b){
+//     if (a < b) { return a; 
+// }
+// return b;
+// }
+// alert(min(1, 1));
+
+// A solution with a question mark operator '?'
+function min(a, b){
+    return (a < b)  ? a : b;
+}
+alert(min(1, 1));
