68d92ffb05907f1d4183e0a2db7753629ade2703
codingstart0 <codingstart0@gmail.com>
2024-02-04 06:57:45 +0200

    shTest13#_Feature#_Add arrow function "ask1" Add arrow function with question mark operator "ask2"

---
+++ JS/short_test/shTest13/script.js
@@ -0,0 +1,32 @@
+// Function declaration
+function ask(question, yes, no) {
+    if (confirm(question)) yes();
+    else no();
+  }
+  
+  ask(
+    "Do you agree?",
+    function() { alert("You agreed."); },
+    function() { alert("You canceled the execution."); }
+  );
+
+// Arrow Function
+let ask1 = (question, yes, no) => {
+    if (confirm(question)) yes();
+    else no();
+  };
+
+ask1(
+  "Do you agree second time?",
+  function() { alert("You agreed."); },
+  function() { alert("You canceled the execution."); }
+);
+
+// Arrow Function with question mark operator
+let ask2 = (question, yes, no) => {(confirm(question)) ? yes() : no();}
+
+ask2(
+  "Do you agree third time?",
+  function() { alert("You agreed."); },
+  function() { alert("You canceled the execution."); }
+);
