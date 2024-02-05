0af711b1019c95f2c9d69340030a398ee353b624
codingstart0 <codingstart0@gmail.com>
2024-02-05 08:37:30 +0200

    shTest14#_Feature#_Add function "capitalize"

--- JS/short_test/shTest14/script.js
+++ JS/short_test/shTest14/script.js
@@ -1,15 +1,24 @@
 let a = prompt('Chose number a');
-a = Number(a);
-
-let b = prompt('Chose number b');
-b = Number(b);
-
+// a = Number(a);
+// This function takes one number and returns that number + 7.
 function add7(a) {
-  return 'add7 value is ' + (a + 7);
+  return 'add7 value is ' + (+a + 7);
 }
 alert(add7(a));
 
+let b = prompt('Chose number b');
+// b = Number(b);
+// This function takes 2 numbers and returns their product.
 function multiply(a, b) {
-  return 'multiply value is ' + a * b;
+  return 'multiply value is ' + +a * b;
 }
 alert(multiply(a, b));
+
+
+let cap = prompt('write a string');
+// This function takes a string and returns that string with the first letter capitalized & the rest of not changed.
+function capitalize() {
+  return cap[0].toUpperCase() + cap.substring(1);
+}
+
+alert(capitalize(cap));
