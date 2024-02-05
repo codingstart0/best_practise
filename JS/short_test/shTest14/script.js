5404dd4b38d4804f0f6078ddf53da66af66497fe
codingstart0 <codingstart0@gmail.com>
2024-02-05 06:15:38 +0200

    shTest14#_Feature#_Add function "multiply"

--- JS/short_test/shTest14/script.js
+++ JS/short_test/shTest14/script.js
@@ -1,7 +1,15 @@
 let a = prompt('Chose number a');
 a = Number(a);
 
+let b = prompt('Chose number b');
+b = Number(b);
+
 function add7(a) {
   return 'add7 value is ' + (a + 7);
 }
 alert(add7(a));
+
+function multiply(a, b) {
+  return 'multiply value is ' + a * b;
+}
+alert(multiply(a, b));
