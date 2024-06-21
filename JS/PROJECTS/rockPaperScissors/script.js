977fe76c4fbfd33993296edc2e9f3487c2ebc105
codingstart0 <codingstart0@gmail.com>
2024-06-21 09:45:02 +0300

    rockPaper#_FEATURES#_Add "playRound" function it take two parameters and compare them - the playerSelection and computerSelection

--- JS/PROJECTS/rockPaperScissors/script.js
+++ JS/PROJECTS/rockPaperScissors/script.js
@@ -11,42 +11,35 @@ choices.forEach((choice) => {
   option.text = choice.charAt(0).toUpperCase() + choice.slice(1); // Capitalize the first letter
   selectElement.appendChild(option);
 });
-// Add the select element to the document body
+// Add the select elem ent to the document body
 document.body.appendChild(selectElement);
 // Add a button to get the player's selection
 const button = document.createElement('button');
 button.textContent = 'Paspausk';
-button.addEventListener('click', getPlayerSelection);
+button.addEventListener('click', playRound);
 document.body.appendChild(button);
 
-function getPlayerSelection() {
+function playRound() {
   const selectedOption = selectElement.value;
   const playerSelection = selectedOption;
-
-  // Generate a new computer selection each time the function is called
-  const computerSelection = choices[Math.floor(Math.random() * choices.length)];
-
   // Add string in web with players choice after player push submit button
   const playerChoice = document.body.appendChild(document.createElement('p'));
   playerChoice.textContent = 'Player selected: ' + playerSelection;
 
+  // Generate a new computer selection each time the function is called
+  const computerSelection = choices[Math.floor(Math.random() * choices.length)];
   // Add string in web with computer choice after player push submit button
   const computerChoice = document.body.appendChild(document.createElement('p'));
   computerChoice.textContent = 'Computer selected: ' + computerSelection;
+
+  if (playerSelection === computerSelection) {
+    const winner = document.createElement('h1');
+    winner.textContent =
+      "It's a TIE - " + playerSelection + ' is equal to ' + computerSelection;
+    document.body.appendChild(winner);
+  }
 }
-
-// Or via prompt player can add playerSelection by string
-// So make shure it's case-insensitive, users can input rock, ROCK, RocK or any other variation.
-// function playerSelection () {
-//     selectElement = document.querySelector(options);
-//     output = selectElement.value;
-//    }
-
-// playRound function should take two parameters - the playerSelection and computerSelection
-// computerSelection should return new value then playerSelection changed
-// Then compare them and return a string that declares the winner. E. g. like so: "You Lose! Paper beats Rock"
-function playRound(playerSelection, computerSelection) {}
 
 // This function play a five round game that keeps score.
 // And reports a winner or loser at the end.
-playGame()
+// playGame()
