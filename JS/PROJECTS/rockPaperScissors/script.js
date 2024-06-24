60db105db0391f57c4915908dda2973517cdb6c5
codingstart0 <codingstart0@gmail.com>
2024-06-24 11:29:41 +0300

    rockPaper#_FEATURES#_Update if else statment

--- JS/PROJECTS/rockPaperScissors/script.js
+++ JS/PROJECTS/rockPaperScissors/script.js
@@ -35,35 +35,34 @@ function playRound() {
   if (playerSelection === computerSelection) {
     const winner = document.createElement('h2');
     const tie = document.createElement('span');
-    tie.textContent = 'TIE';
+    tie.textContent = 'TIE ';
     tie.classList.add('tie-color'); // Add the 'tie-color' class to style the text
 
     winner.innerHTML =
       "It's a " +
       tie.outerHTML +
-      ' ' +
       playerSelection +
       ' is equal to ' +
       computerSelection;
     document.body.appendChild(winner);
-  }
-    else if (playerSelection === 'rock' & computerSelection === 'scissors') {
-      const winner = document.createElement('h2');
-      const win = document.createElement('span');
-      win.textContent = 'WIN ';
-      win.classList.add('win-color'); // Add the 'win-color' class to style the text
+  } 
   
-      winner.innerHTML =
-        "You " +
-        tie.outerHTML +
-        playerSelection +
-        ' beats ' +
-        computerSelection;
-      document.body.appendChild(winner);
-    }
-  }
-  //   else if (playerSelection === 'rock' & computerSelection === 'paper')}
+  else if (
+    (playerSelection === 'Rock') &
+    (computerSelection === 'Scissors')
+  ) {
+    const winner = document.createElement('h2');
+    const win = document.createElement('span');
+    win.textContent = 'WIN ';
+    win.classList.add('win-color'); // Add the 'win-color' class to style the text
+    document.body.appendChild(winner);
 
+    winner.innerHTML =
+      'You ' + win.outerHTML + playerSelection + ' beats ' + computerSelection;
+    document.body.appendChild(winner);
+  }
+}
+//   else if (playerSelection === 'rock' & computerSelection === 'paper')}
 
 // This function play a five round game that keeps score.
 // And reports a winner or loser at the end.
