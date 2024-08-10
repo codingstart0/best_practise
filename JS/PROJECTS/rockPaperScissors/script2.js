27d681aa5621fa493e5918ea148cbe8d4b41fb98
codingstart0 <codingstart0@gmail.com>
2024-08-10 08:09:07 +0300

    rockPaper#Feature#_Add scoring mechanizm

--- JS/PROJECTS/rockPaperScissors/script2.js
+++ JS/PROJECTS/rockPaperScissors/script2.js
@@ -4,6 +4,10 @@ const choices = ["rock", "paper", "scissors"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let playerScore = 0;
+let computerScore = 0;
 
 function playGame(playerChoise){
 
@@ -39,9 +43,13 @@ switch(result){
         break;
     case "YOU WIN!":
         resultDisplay.classList.add("greenText");
+        playerScore ++;
+        playerScoreDisplay.textContent = playerScore;
         break;
     case "YOU LOSE!":
         resultDisplay.classList.add("redText");
+        computerScore ++;
+        computerScoreDisplay.textContent = computerScore;
         break;
 }
 }
