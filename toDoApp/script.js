793e159ee81f78d00fe9c13a559e0ffa36e794d1
codingstart0 <codingstart0@gmail.com>
2024-11-06 15:34:57 +0200

    Uncomment the getExistingTodos function

--- toDoApp/script.js
+++ toDoApp/script.js
@@ -23,11 +23,11 @@ function loadTodos() {
   }
 }
 
-// function getExistingTodos() {
-//   return Array.from(
-//     document.querySelectorAll('#todo-list .form-check-label')
-//   ).map((label) => label.textContent.trim());
-// }
+function getExistingTodos() {
+  return Array.from(
+    document.querySelectorAll('#todo-list .form-check-label')
+  ).map((label) => label.textContent.trim());
+}
 
 function addTodo() {
   const input = document.getElementById('todo-input');
